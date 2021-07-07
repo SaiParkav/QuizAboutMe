492fb824c6183137b0c2667b818cc2487e7a3a72
SaiParkav <86610494+SaiParkav@users.noreply.github.com>
2021-07-07 16:03:02 +0530

    Create QuizAboutMe.js

---
+++ QuizAboutMe.js
@@ -0,0 +1,61 @@
+
+const readlinesync=require('readline-sync')
+const chalk=require('chalk')
+
+let score=0;
+let playerName=readlinesync.question(chalk.bold.yellow("May I know your name ? "));
+
+console.log(chalk.bold.blue("Welcome "+ playerName + " to DO YOU KNOW PARKAV ?"))
+
+function play(question,ans,options)
+{
+  console.log("Question ");
+  console.log(chalk.bold.yellow(question));
+  let playerAnswer=readlinesync.keyInSelect(options);
+  if((playerAnswer+1) === ans){
+    console.log(chalk.green("You are right"));
+    score++;
+  }
+  else
+  {
+    console.log(chalk.red("You are wrong"));
+  }
+
+  console.log("Your current score is" ,score);
+  console.log("------------------");
+}
+
+
+const questions=[{
+  ques:"Where do I live ? ",
+  ans : 3,
+  options:["Raipur","Delhi","Hyderabad","Bangalore"]
+},
+{
+  ques:"Where do i study?",
+  ans:2,
+  options:["VIT","SRM","Manipal","BITS"]
+},
+{
+  ques:"What do i study?",
+  ans:4,
+  options:["ECE","Mechatronics","Automobile","CSE"]
+},
+{
+  ques:"What pet do i have?",
+  ans:4,
+  options:["Fish","Cat","Dog","None"]
+},
+{
+  ques:"Do i like gaming?",
+  ans:1,
+  options:["Yes","No"]
+},];
+
+
+
+for( let i=0;i<questions.length;i++){
+  play(questions[i].ques,questions[i].ans,questions[i].options)
+}
+
+console.log("Congratulations Your final score is", score);
